02f9cd142bbcd753d60fb72c190c8bd43cbde965
Stevan <slavnic.stevan@gmail.com>
2021-01-24 17:09:45 +0100

    Changed optimisation in webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -60,22 +60,8 @@ module.exports = {
     path: path.resolve(__dirname, 'build')
   },
   optimization: {
-    runtimeChunk: 'single',
     splitChunks: {
-      chunks: 'all',
-      maxInitialRequests: Infinity,
-      minSize: 0,
-      cacheGroups: {
-        vendor: {
-          test: /[\\/]node_modules[\\/]/,
-          name(module) {
-            const packageName = module.context.match(
-              /[\\/]node_modules[\\/](.*?)([\\/]|$)/
-            )[1]
-            return `npm.${packageName.replace('@', '')}`
-          }
-        }
-      }
+      chunks: 'all'
     }
   },
   plugins: [
